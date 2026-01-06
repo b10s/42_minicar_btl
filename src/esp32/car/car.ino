460b9ad72d0d312a2e8e248de9c2607456d0d3f3
George O <georgyostr@gmail.com>
2026-01-07 01:43:29 +0900

    untested car.ino script, moving motor, moving servo

--- src/esp32/car/car.ino
+++ src/esp32/car/car.ino
@@ -1,98 +1,55 @@
-// ============================================================================
-//  LD06 LiDAR – ESP32 Example Sketch
-// ============================================================================
-
 #include "ld06.h"
 #include <ESP32Servo.h>
 
-// ---------------- ESP32 UART CONFIG ----------------
+// ESP32 UART config for lidar ld 06
 #define LD06_RX_PIN 16   // ESP32 RX2
 #define LD06_TX_PIN 17   // ESP32 TX2
-#define LED_BUILTIN 2
+
 #define MOTOR_PIN 5
 #define SERVO_PIN 4
 
+// Values in microseconds (us)
+#define SERVO_NEUTRAL		1480		   					// straight
+#define SERVO_MIN			1240    	       				// full left
+#define SERVO_MAX			1720        	       			// full right
+#define SERVO_RANGE_LEFT	(PULSE_NEUTRAL - PULSE_MIN)
+#define SERVO_RANGE_RIGHT	(PULSE_MAX - PULSE_NEUTRAL)
 
-#define PULSE_NEUTRAL 1480               // straight
-#define PULSE_MIN 1240                   // full left
-#define PULSE_MAX 1720                   // full right
-#define PULSE_RANGE_LEFT (PULSE_NEUTRAL - PULSE_MIN)   // 320 us
-#define PULSE_RANGE_RIGHT (PULSE_MAX - PULSE_NEUTRAL)  // 160 us
+#define MOTOR_NEUTRAL		1560							// Neutral
+#define MOTOR_NEUTRAL_BACK	1480							// After that can go backward
+#define MOTOR_MIN			1080							// Full speed
+#define MOTOR_MAX			2000							// Full back/ brake
+#define MOTOR_RANGE_BACK	(PULSE_NEUTRAL - PULSE_MIN)
+#define MOTOR_RANGE_FRONT	(PULSE_MAX - PULSE_NEUTRAL) 
 
-Servo srv;
+#define CYCLE				17021							// PWM Cycle for motor and servo
+
+Servo servo;
 Servo motor;
 
 HardwareSerial LidarSerial(2); // UART2
 LD06 ld06(LidarSerial);
 
-// --------------------------------------------------
-void toggleBuiltinLed() {
-#ifdef LED_BUILTIN
-  static bool ledState = false;
-  static uint32_t last = 0;
-  if (millis() - last > 300) {
-    last = millis();
-    ledState = !ledState;
-    digitalWrite(LED_BUILTIN, ledState);
-  }
-#endif
-}
-
-void servoTurn(float pos, int timeMs) {  
-  int rng;
-
-  if(pos < -100.)
-    pos = -100.;
-  if(pos > 100.)
-    pos = 100.;
-  if (pos < 0)
-    rng = PULSE_RANGE_LEFT;
-  else
-    rng = PULSE_RANGE_RIGHT;
-  float peak = pos * rng / 100;
-  srv.writeMicroseconds(PULSE_NEUTRAL + peak);
-  delay(timeMs);
-}
-
-void motorRun(float pos) {  
-  int rng;
-
-  if(pos < -100.)
-    pos = -100.;
-  if(pos > 100.)
-    pos = 100.;
-  if (pos < 0)
-    rng = PULSE_RANGE_LEFT;
-  else
-    rng = PULSE_RANGE_RIGHT;
-  float peak = pos * rng / 100;
-  motor.writeMicroseconds(PULSE_NEUTRAL + peak);
-}
-
 int start = 0;
-// --------------------------------------------------
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  srv.attach(SERVO_PIN, PULSE_MIN, PULSE_MAX);
-  srv.write(PULSE_NEUTRAL);
-  motor.attach(MOTOR_PIN, PULSE_MIN, PULSE_MAX);
-  motor.write(PULSE_NEUTRAL);
-  delay(2000);
-
-  motorRun(50);
-  servoTurn(25, 1000);
-  servoTurn(50, 1000);
-  motorRun(100);
-  delay(2000);
-  motorRun(0);
+int c = 0;
   
-  //motor.write(PULSE_NEUTRAL + (PULSE_MAX-PULSE_NEUTRAL)/8);
-  //delay(1000);
-  //motor.write(PULSE_MIN);
-  //delay(100);
-  //motor.write(PULSE_NEUTRAL);
-  // USB debug output
+void setup() {
+    
+  servo.attach(SERVO_PIN, SERVO_MIN, SERVO_MAX);
+  servo.write(SERVO_NEUTRAL);
+  motor.attach(MOTOR_PIN, MOTOR_MIN, MOTOR_MAX);
+  motor.write(MOTOR_NEUTRAL);
+  
+  delay(2000);
+  motor.write(MOTOR_MAX);
+  delay(2000);
+  motor.write(MOTOR_NEUTRAL);
+
+  delay(2000);
+  servo.write(SERVO_MAX);
+  delay(2000);
+  servo.write(SERVO_NEUTRAL)
+  
   Serial.begin(921600);
   delay(1000);
   Serial.println("LD06 ESP32 starting...");
@@ -113,31 +70,27 @@ void setup() {
   ld06.setDistanceRange(100, 8000); // 0.1m – 8m
   ld06.setAngleRange(0, 360);
   start = millis();
-
 }
 
-int   c = 0;
-// --------------------------------------------------
+
+
 void loop() {
-  toggleBuiltinLed();
-
   // Read LiDAR scan
+
   if (ld06.readScan()) {
-
     // --- Real-time visualization ---
-    //Serial.printf(">U:|clr\\n");
-    //Serial.printf(">U:|clr\n");
-    Serial.printf(">!:|clr\n"); 
-    ld06.printScanTeleplot(Serial);
+    // Serial.printf(">!:|clr\n"); 
+    // ld06.printScanTeleplot(Serial);
     
-    //Serial.print("Speed (deg/s): ");
-    //Serial.println(ld06.getSpeed(), 1);
+    Serial.print("Speed (deg/s): ");
+    Serial.println(ld06.getSpeed(), 1);
 
-    //Serial.print("Points: ");
-    //Serial.println(ld06.getNbPointsInScan());
-    //c+=1;
-    //Serial.print((millis()-start)/c);
+    Serial.print("Points: ");
+    Serial.println(ld06.getNbPointsInScan());
     
+	  c+=1;
+	  Serial.print("Hz: ");
+    Serial.println((millis() - start) / c)
   }
 }
 
